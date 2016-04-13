aa6ce4e3c7dd1db025fba657f39b5e79454339f8
fengliya <fengliya@tuniu.com>
2016-04-13 10:28:37 +0800

    双偶数阶
    增加双偶数阶

--- jiugongge.playground/Contents.swift
+++ jiugongge.playground/Contents.swift
@@ -2,22 +2,22 @@
 
 import UIKit
 
-var num = 5
+var num = 8
 
 var arrays = Array<Array<Int>>()
-var numbers = Array<Int>()
-var preI = 0
-var preJ = 0
 
-for var i = 0; i < num; i++ {
-    var tmpArray = Array<Int>()
-    for var j = 0; j < num; j++ {
-        tmpArray.append(0)
+if num % 2 != 0 { //奇数阶幻方
+    
+    for var i = 0; i < num; i++ { //初始化为0
+        var tmpArray = Array<Int>()
+        for var j = 0; j < num; j++ {
+            tmpArray.append(0)
+        }
+        arrays.append(tmpArray)
     }
-    arrays.append(tmpArray)
-}
-
-if num % 2 != 0 {
+    
+    var preI = 0
+    var preJ = 0
     arrays[0][num / 2] = 1;
     preI = 0
     preJ = num / 2
@@ -33,10 +33,33 @@ if num % 2 != 0 {
         preJ = currentJ
     }
 }
-else {
+else if num % 4 == 0 { //双偶数阶幻方
     
-}
+    for var i = 0; i < num; i++ { //初始化为0
+        var tmpArray = Array<Int>()
+        for var j = 0; j < num; j++ {
+            tmpArray.append(i * num + j + 1)
+        }
+        arrays.append(tmpArray)
+    }
+    
+    let totalCount = num * num + 1
+    let divide = num / 4
+    let divideArraysNum = divide * divide
+    
+    for var n = 0; n < divideArraysNum; n++ {
+        var baseI = n / divide
+        var baseJ = n % divide
 
+        for var i = 0; i < 4; i++ {
+            for var j = 0; j < 4; j++ {
+                if ((i == j) || (i == 4 - j - 1)) {
+                    arrays[baseI * 4 + i][baseJ * 4 + j] = totalCount - arrays[baseI * 4 + i][baseJ * 4 + j]
+                }
+            }
+        }
+    }
+}
 
 for var i = 0; i < num; i++ {
     var tmpStr = ""
